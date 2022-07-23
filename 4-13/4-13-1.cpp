966fec70791bc293be803247f72d5b4a09cfb58d
Wangyxht <wangyx0001@qq.com>
2022-07-23 11:58:35 +0800

    C++PrimePlus 习题

---
+++ 4-13/4-13-1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+
+int main(void)
+{
+
+    using namespace std;
+
+    string firstName;
+    string secondName;
+    string out;
+    char gardeLetter;
+    int age;
+
+    cout<<"What is your first name:";
+    getline(cin,firstName);
+    
+    cout<<"What is your second name:";
+    getline(cin,secondName);
+
+    cout<<"What is your age:";
+    cin>>age;
+    cin.get();
+
+    cout<<"What letter grade do you deserve:";
+    cin>>gardeLetter;
+    cin.get();
+
+    gardeLetter+=1;
+
+    out=secondName+","+" "+firstName;
+    cout<<"Name:"<<out<<endl;
+    cout<<"Grade:"<<gardeLetter<<endl;
+    cout<<"Age:"<<age<<endl;
+
+    return 0;
+    
+}
